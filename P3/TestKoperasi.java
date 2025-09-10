2f6967ac115cb7f078e1764a5e5fbe53eaf0c725
YanuarRamadhaniKuswoko <ryanuar867@gmail.com>
2025-09-10 21:16:30 +0700

    P3-Tugas-Pertanyaan5

--- P3/TestKoperasi.java
+++ P3/TestKoperasi.java
@@ -1,27 +1,25 @@
 package P3;
 
+import java.util.Scanner;
+
 public class TestKoperasi {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         AnggotaTugas donny = new AnggotaTugas("111333444", "Donny", 5000000);
 
         System.out.println("Nama Anggota: " + donny.getNama());
         System.out.println("Limit Pinjaman: " + donny.getLimitPinjaman());
 
-        System.out.println("\nMeminjam uang 10.000.000...");
-        donny.pinjam(10000000);
+        System.out.print("\nMasukkan jumlah pinjaman: ");
+        int pinjaman = sc.nextInt();
+        donny.pinjam(pinjaman);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
 
-        System.out.println("\nMeminjam uang 4.000.000...");
-        donny.pinjam(4000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+        System.out.print("\nMasukkan jumlah angsuran: ");
+        int angsuran = sc.nextInt();
+        donny.angsur(angsuran);
+        System.out.println("Jumlah pinjaman setelah angsuran: " + donny.getJumlahPinjaman());
 
-        System.out.println("\nMembayar angsuran 1.000.000");
-        donny.angsur(1000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-
-        System.out.println("\nMembayar angsuran 3.000.000");
-        donny.angsur(3000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
     }
 }
-
